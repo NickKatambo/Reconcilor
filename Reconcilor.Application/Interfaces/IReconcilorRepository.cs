a39a50ca2b5f899bdaa7677ed4172410c4d5f9c7
NickKatambo <yannick.katambo@outlook.com>
2024-07-27 23:32:39 +0200

    define repository and service

---
+++ Reconcilor.Application/Interfaces/IReconcilorRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Reconcilor.Application.Interfaces
+{
+    public interface IReconcilorRepository
+    {
+        Task<IQueryable<T>> GetDataAsync<T>();
+        Task<T> GetDataAsync<T>(int id);
+        Task<T> SaveDataAsync<T>(T entity);
+        Task<T> UpdateDataAsync<T>(int id,T entity);
+        Task<T> DeleteDataAsync<T>(int id);
+    }
+}
---
+++ Reconcilor.Application/Services/ReconcilorService.cs
@@ -0,0 +1,32 @@
+﻿using Reconcilor.Application.Interfaces;
+
+namespace Reconcilor.Application.Services
+{
+    public class ReconcilorService : IReconcilorRepository
+    {
+        public Task<T> DeleteDataAsync<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<T>> GetDataAsync<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetDataAsync<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> SaveDataAsync<T>(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateDataAsync<T>(int id, T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
